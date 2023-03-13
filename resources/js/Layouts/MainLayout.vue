3d2143f87bddae8902546490aef501bc4d06f2c5
Shotz5 <kitsulalex@gmail.com>
2023-03-13 00:36:07 -0700

    S.6.31: Reactive data in Vue

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,9 +1,15 @@
 <template>
     <Link href="/">Main Page</Link>&nbsp;
     <Link href="/show">Show Page</Link>
+    <div>The page with time {{ timer }}</div>
     <slot></slot>
 </template>
 
 <script setup>
     import {Link} from '@inertiajs/vue3'
+    import {ref} from 'vue'
+
+    const timer = ref(0)
+    // Built in JS function, can be defined here apparently
+    setInterval(() => timer.value++, 1000)
 </script>
