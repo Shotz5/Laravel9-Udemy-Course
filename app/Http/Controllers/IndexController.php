9f1d05708cd217595f14555da41875adf479e5b7
Shotz5 <kitsulalex@gmail.com>
2023-03-13 02:00:14 -0700

    S.7.42: Querying the database

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Listing;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
     public function index()
     {
+        dd(Listing::
+            where('beds', '>', 4)
+            ->where('area', '>', 100)
+            ->orderBy('beds')
+            ->get()
+        );
         return inertia('Index/Index', [
             'message' => 'Hello from Laravel',
         ]);
