93a2967bedace875d64da295336b8d0b41883658
Shotz5 <kitsulalex@gmail.com>
2023-03-13 22:33:49 -0700

    S.7.47: Handling forms (useForm, v-model)

---
+++ resources/js/Pages/Listing/Create.vue
@@ -0,0 +1,74 @@
+<template>
+    <form @submit.prevent="create">
+        <div>
+            <label>Beds</label>
+            <input type="text" v-model.number="form.beds" />
+        </div>
+
+        <div>
+            <label>Baths</label>
+            <input type="text" v-model.number="form.baths" />
+        </div>
+
+        <div>
+            <label>Area</label>
+            <input type="text" v-model.number="form.area" />
+        </div>
+
+        <div>
+            <label>City</label>
+            <input type="text" v-model="form.city" />
+        </div>
+
+        <div>
+            <label>Postal Code</label>
+            <input type="text" v-model="form.code" />
+        </div>
+
+        <div>
+            <label>Street</label>
+            <input type="text" v-model="form.street" />
+        </div>
+
+        <div>
+            <label>Street Number</label>
+            <input type="text" v-model.number="form.street_nr" />
+        </div>
+
+        <div>
+            <label>Price</label>
+            <input type="text" v-model.number="form.price" />
+        </div>
+
+        <div>
+            <button type="submit">Create</button>
+        </div>
+    </form>
+</template>
+
+<script setup>
+    import { useForm } from '@inertiajs/vue3'
+
+    const form = useForm({
+        beds: 0,
+        baths: 0,
+        area: 0,
+        city: null,
+        street: null,
+        code: null,
+        street_nr: 0,
+        price: 0,
+    })
+
+    const create = () => form.post('/listing', form)
+</script>
+
+<style scoped>
+label {
+    margin-right: 2em;
+}
+
+div {
+    padding: 2px;
+}
+</style>
