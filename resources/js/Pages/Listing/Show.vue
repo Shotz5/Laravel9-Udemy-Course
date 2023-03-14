2fcba12e8775750882800940e7ac87e05f1be123
Shotz5 <kitsulalex@gmail.com>
2023-03-13 23:56:23 -0700

    S.7.52: Edit Form

--- resources/js/Pages/Listing/Show.vue
+++ resources/js/Pages/Listing/Show.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script setup>
-    import ListingAddress from '@/Components/LIstingAddress.vue';
+    import ListingAddress from '@/Components/ListingAddress.vue';
     defineProps({
         listing: Object,
     })
