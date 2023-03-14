5dcdfb720036ce6be5c9fa7e797e0779aef3f68f
Shotz5 <kitsulalex@gmail.com>
2023-03-13 22:53:17 -0700

    S.7.49: Middlewares and Sharing Data with All Pages

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,15 +1,10 @@
 <template>
     <Link href="/">Main Page</Link>&nbsp;
     <Link href="/show">Show Page</Link>
-    <div>The page with time {{ timer }}</div>
     <slot>Default</slot>
 </template>
 
 <script setup>
     import {Link} from '@inertiajs/vue3'
-    import {ref} from 'vue'
 
-    const timer = ref(0)
-    // Built in JS function, can be defined here apparently
-    setInterval(() => timer.value++, 1000)
 </script>
