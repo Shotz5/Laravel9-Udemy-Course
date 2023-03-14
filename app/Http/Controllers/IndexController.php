a487eb56970026a44b1488bc11646fd420f357c2
Shotz5 <kitsulalex@gmail.com>
2023-03-13 21:23:59 -0700

    S.7.45: Resource controller and route model binding

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -9,32 +9,6 @@ class IndexController extends Controller
 {
     public function index()
     {
-        // $listing = Listing::find(10);
-        // $listing->city = 'Tinker';
-        // $listing->save();
-
-        // $listing = new Listing;
-        // $listing->beds = 1;
-        // $listing->baths = 1;
-        // $listing->area = 50;
-        // $listing->city = "London";
-        // $listing->street = "Tinker Street";
-        // $listing->street_nr = "10";
-        // $listing->code = "TK";
-        // $listing->price = 100_000;
-        // $listing->save();
-
-        // $listing = Listing::create([
-        //     "beds" => 2,
-        //     "baths" => 2,
-        //     "area" => 100,
-        //     "city" => "North",
-        //     "street" => "Tinker St",
-        //     "street_nr" => 20,
-        //     "code" => "TS",
-        //     "price" => 200,000
-        // ]);
-
         return inertia('Index/Index', [
             'message' => 'Hello from Laravel',
         ]);
