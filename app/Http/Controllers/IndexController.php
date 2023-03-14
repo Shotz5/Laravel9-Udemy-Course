56337413b7f76266b2827196e7e60f3d0094076a
Shotz5 <kitsulalex@gmail.com>
2023-03-13 21:11:00 -0700

    S.7.44: Updating and Inserting Data, Mass Assignment

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -9,11 +9,32 @@ class IndexController extends Controller
 {
     public function index()
     {
-        dd(Listing::
-            where('beds', '>=', 4)
-            ->orderBy('price', 'asc')
-            ->first()
-        );
+        // $listing = Listing::find(10);
+        // $listing->city = 'Tinker';
+        // $listing->save();
+
+        // $listing = new Listing;
+        // $listing->beds = 1;
+        // $listing->baths = 1;
+        // $listing->area = 50;
+        // $listing->city = "London";
+        // $listing->street = "Tinker Street";
+        // $listing->street_nr = "10";
+        // $listing->code = "TK";
+        // $listing->price = 100_000;
+        // $listing->save();
+
+        // $listing = Listing::create([
+        //     "beds" => 2,
+        //     "baths" => 2,
+        //     "area" => 100,
+        //     "city" => "North",
+        //     "street" => "Tinker St",
+        //     "street_nr" => 20,
+        //     "code" => "TS",
+        //     "price" => 200,000
+        // ]);
+
         return inertia('Index/Index', [
             'message' => 'Hello from Laravel',
         ]);
