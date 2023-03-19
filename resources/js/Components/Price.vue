d6f2f5943bd51f12a55607122913be3d4534f264
Shotz5 <kitsulalex@gmail.com>
2023-03-18 19:39:28 -0700

    S.8.61: Custom Price Formatting Component

---
+++ resources/js/Components/Price.vue
@@ -0,0 +1,19 @@
+<template>
+    <span>{{ formattedPrice }}</span>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+
+const props = defineProps({
+    price: [Number, String],
+})
+
+const formattedPrice = computed(
+    () => Number(props.price).toLocaleString('en-CA', {
+        style: 'currency',
+        currency: 'CAD',
+        maximumFractionDigits: 0,
+    }),
+)
+</script>
