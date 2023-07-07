0e35f57253736c4f55dc235998f3d1a425a32c6f
Alex Kitsul <kitsulalex@gmail.com>
2023-07-06 17:43:21 -0700

    S.9.73: Adding more composable members (total paid/total interest)

--- resources/js/Pages/Listing/Show.vue
+++ resources/js/Pages/Listing/Show.vue
@@ -22,14 +22,36 @@
                     type="range" min="0.1" max="30" step="0.1"
                     class="w-full h-4 bg-gray-200 rounded-lg appearence-none cursor-pointer dark:bg-gray-700"
                 />
+
                 <label>Duration ({{ duration }} years)</label>
                 <input v-model.number="duration"
                     type="range" min="3" max="35" step="1"
                     class="w-full h-4 bg-gray-200 rounded-lg appearence-none cursor-pointer dark:bg-gray-700"
                 />
+
                 <div class="text-gray-600 dark:text-gray-300 mt-2">
-                    <div class="text-gray-400">
-                        <Price :price="monthlyPayment" class="text-3xl"></Price>
+                    <div class="text-gray-400">Your monthly payment</div>
+                    <Price :price="monthlyPayment" class="text-3xl"></Price>
+                </div>
+
+                <div class="mt-2 text-gray-500">
+                    <div class="flex justify-between">
+                        <div>Total Paid</div>
+                        <div>
+                            <Price :price="totalPaid" class="font-medium" />
+                        </div>
+                    </div>
+                    <div class="flex justify-between">
+                        <div>Principle Paid</div>
+                        <div>
+                            <Price :price="listing.price" class="font-medium" />
+                        </div>
+                    </div>
+                    <div class="flex justify-between">
+                        <div>Interest Paid</div>
+                        <div>
+                            <Price :price="totalInterest" class="font-medium" />
+                        </div>
                     </div>
                 </div>
             </Box>
@@ -54,5 +76,5 @@
         listing: Object,
     });
 
-    const { monthlyPayment } = useMonthlyPayment(props.listing.price, interestRate, duration);
+    const { monthlyPayment, totalPaid, totalInterest } = useMonthlyPayment(props.listing.price, interestRate, duration);
 </script>
